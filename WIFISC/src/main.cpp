50b8f165c179804c71da6cae45c9955b689be3cd
silvajhb <jhbs.student@gmail.com>
2021-09-07 01:11:18 -0300

    Upload Files

--- WIFISC/src/main.cpp
+++ WIFISC/src/main.cpp
@@ -1,9 +1,152 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+/*
+ *  This sketch demonstrates how to scan WiFi networks.
+ *  The API is almost the same as with the WiFi Shield library,
+ *  the most obvious difference being the different file you need to include:
+ */
+#include "WiFi.h"
+//---WifiServer
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+//-------------------------------------------------------------------------------
+
+//--webserver
+const char* ssid = "";                     // your network SSID (name)
+const char* pass = "";       // your network key
+int keyIndex = 0;                                // your network key Index number
+int status = WL_IDLE_STATUS;                     // the Wifi radio's status
+WebServer server(80);
+const int led = 13;
+
+//--WifiScan---------------------------------------------------------------------
+void WifiScanSetup(){
+    // Set WiFi to station mode and disconnect from an AP if it was previously connected
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
+}
+void WifiScanLoop(){
+    Serial.println("scan start");
+    // WiFi.scanNetworks will return the number of networks found
+    int n = WiFi.scanNetworks();
+    Serial.println("scan done");
+    if (n == 0) {
+        Serial.println("no networks found");
+    } else {
+        Serial.print(n);
+        Serial.println(" networks found");
+        for (int i = 0; i < n; ++i) {
+            // Print SSID and RSSI for each network found
+            Serial.print(i + 1);
+            Serial.print(": ");
+            Serial.print(WiFi.SSID(i));
+            Serial.print(" (");
+            Serial.print(WiFi.RSSI(i));
+            Serial.print(")");
+            Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+            delay(10);
+        }
+    }
+    Serial.println("");
+
+    // Wait a bit before scanning again
+    delay(5000);
+}
+//-------------------------------------------------------------------------------
+
+
+//--WebServer--------------------------------------------------------------------
+void handleRoot() {
+  digitalWrite(led, 1);
+  server.send(200, "text/plain", "hello from esp32!");
+  digitalWrite(led, 0);
+}
+void handleNotFound() {
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
+}
+void WifiServerSetup(){
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 0);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp32")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+
+  server.on("/inline", []() {
+    server.send(200, "text/plain", "this works as well");
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+//-------------------------------------------------------------------------------
+void setup(){
+    Serial.begin(115200);
+
+      while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true);
+  }
+
+  // attempt to connect to Wifi network:
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WEP network, SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin("ssid", "keyIndex", "pass");
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  // once you are connected :
+  Serial.print("You're connected to the network");
+    
+    WifiScanSetup();
+    WifiServerSetup();
+    Serial.println("Setup done");
+    
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop(){
+   Serial.println(WiFi.status());
