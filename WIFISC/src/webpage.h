f58e9db6112fdbf23b99eb423bc702df04fe626d
silvajhb <jhbs.student@gmail.com>
2021-09-12 17:38:43 -0300

    refactor webpage

---
+++ WIFISC/src/webpage.h
@@ -0,0 +1,23 @@
+R"(<html>
+<head>
+    <meta http-equiv='refresh' content='5' />
+    <title>ESP32 Demo</title>
+    <style>
+        
+ body {
+            background-color: #cccccc;
+            font-family: Arial, Helvetica, Sans-Serif;
+            Color: #000088;
+        }
+
+        
+    </style>
+</head>
+
+<body>
+    <h1>Hello from ESP32!</h1>
+    <p>Uptime: %02d:%02d:%02d</p>
+</body>
+
+</html>
+)"
