45bd0b5109727bda3118033dcaa63ea14dbf0a2e
silvajhb <jhbs.student@gmail.com>
2021-09-12 12:11:11 -0300

    adjust

--- WIFISC/src/main.cpp
+++ WIFISC/src/main.cpp
@@ -1,12 +1,4 @@
 #include <Arduino.h>
-
-/*
- *  This sketch demonstrates how to scan WiFi networks.
- *  The API is almost the same as with the WiFi Shield library,
- *  the most obvious difference being the different file you need to include:
- */
-#include "WiFi.h"
-//---WifiServer
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WebServer.h>
