6bff20d53f373810f4c213e44d1cd315571dbfbe
silvajhb <jhbs.student@gmail.com>
2021-09-12 15:07:34 -0300

    add http Advanced Auth

--- WIFISC/src/main.cpp
+++ WIFISC/src/main.cpp
@@ -3,11 +3,17 @@
 //--WifiIncludes------------------------------------------------------------------
 #include <WiFi.h>
 //--WifiScanIncludes-------------------------------------------------------------
-
+#include <WiFi.h>
 //--WifiServerIncludes-----------------------------------------------------------
+#include <WiFi.h>
 #include <WiFiClient.h>
 #include <WebServer.h>
 #include <ESPmDNS.h>
+//--HttpAdvancedAuthIncludes-----------------------------------------------------
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <ArduinoOTA.h>
+#include <WebServer.h>
 //##EndIncludes##################################################################
 
 
@@ -30,6 +36,13 @@ String msgtxt; //creating variable to traffic text messages;
 const char *ssid = "henrique";
 const char *password = "grampovoador";
 WebServer server(80);
+//--httpAdvancedAuthVariables----------------------------------------------------
+const char* www_username = "admin";
+const char* www_password = "esp32";
+// allows you to set the realm of authentication Default:"Login Required"
+const char* www_realm = "Custom Auth Realm";
+// the Content of the HTML response in case of Unautherized Access Default:empty
+String authFailResponse = "Authentication Failed";
 //++EndVariables+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 //==StartFuncions================================================================
@@ -178,16 +191,51 @@ void WifiServerLoop(){
   server.handleClient();
   delay(2); //allow the cpu to switch to other tasks
 }
+//__httpAdvancedAuthFunctions____________________________________________________
+void HttpAdvancedAuthSetup(){
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("WiFi Connect Failed! Rebooting...");
+    delay(1000);
+    ESP.restart();
+  }
+  ArduinoOTA.begin();
+
+  server.on("/", []() {
+    if (!server.authenticate(www_username, www_password))
+      //Basic Auth Method with Custom realm and Failure Response
+      //return server.requestAuthentication(BASIC_AUTH, www_realm, authFailResponse);
+      //Digest Auth Method with realm="Login Required" and empty Failure Response
+      //return server.requestAuthentication(DIGEST_AUTH);
+      //Digest Auth Method with Custom realm and empty Failure Response
+      //return server.requestAuthentication(DIGEST_AUTH, www_realm);
+      //Digest Auth Method with Custom realm and Failure Response
+    {
+      return server.requestAuthentication(DIGEST_AUTH, www_realm, authFailResponse);
+    }
+    server.send(200, "text/plain", "Login OK");
+  });
+  server.begin();
+
+  Serial.print("Open http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/ in your browser to see it working");
+}
+void HttpAdvancedAuthLoop(){
+  ArduinoOTA.handle();
+  server.handleClient();
+  delay(2);//allow the cpu to switch to other tasks
+}
 //==EndFuncions==================================================================
 
 void setup(){
   pinMode(LedStatus, OUTPUT);
   digitalWrite(LedStatus, 0);
   Serial.begin(115200);
-  WifiServerSetup();
+  HttpAdvancedAuthSetup();
 }
 
 void loop(){
-  WifiServerLoop();
-
+  HttpAdvancedAuthLoop();
 }
